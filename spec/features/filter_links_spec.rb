5f2135a530a20cb71d96e1ce1388348eea5b69fb
samover <samuel.overloop@gmail.com>
2015-11-04 17:52:59 +0000

    features test filter by tag

--- spec/features/filter_links_spec.rb
+++ spec/features/filter_links_spec.rb
@@ -1,10 +1,15 @@
 feature 'Filter links' do
+  before(:each) do
+    Link.create(title: 'Makers Academy', url: 'www.makersacadamy.com', tags: [Tag.first_or_create(name: 'bootcamps')])
+    Link.create(title: 'BBC', url: 'www.bbc.co.uk', tags: [Tag.first_or_create(name: 'news')])
+    Link.create(title: 'Google', url: 'www.google.com', tags: [Tag.first_or_create(name: 'searcher')])
+    Link.create(title: 'I love bubbles', url: 'www.bubblsrus.com', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
   scenario 'filter links by tags' do
-    visit '/links/new'
-    fill_in 'title', with: 'I love bubbles'
-    fill_in 'url', with: 'www.bubblesrus.com'
-    fill_in 'name', with: 'bubbles'
-    click_button 'Submit'
     visit '/tags/bubbles'
+    expect(page).not_to have_content('BBC')
+    expect(page).not_to have_content('Google')
+    expect(page).not_to have_content('Makers Academy')
+    expect(page).to have_content('I love bubbles')
   end
 end
