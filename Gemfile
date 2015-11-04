cd9e1693b347ff574c2cf89c07eb73168786b67e
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-11-04 12:11:32 +0000

    makes sinatra app modular

--- Gemfile
+++ Gemfile
@@ -2,12 +2,11 @@ source "https://rubygems.org"
 ruby '2.2.3'
 
 gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 
 group :test do
   gem 'capybara'
   gem 'rspec'
   gem 'rubocop'
-  gem 'data_mapper'
-  gem 'dm-postgres-adapter'
 end
-
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,11 +1,13 @@
 require './app/models/link'
+require 'spec_helper'
+
 
 feature 'View Links' do
   scenario 'it shows stored links on homepage' do
     Link.create(title: 'google', url: 'www.google.com')
     visit '/'
     click_button 'View Bookmarks'
-    within("ul#links") do
+    within 'ul#links' do
       expect(page).to have_content("www.google.com")
     end
   end
