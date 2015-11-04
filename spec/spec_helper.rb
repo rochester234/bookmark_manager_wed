579e937f293fe89aa7889f190b43427a8e4b41d5
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-11-04 14:37:06 +0000

    test/development mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+ENV['RACK_ENV'] = 'test'
 require 'capybara'
 require 'rspec'
 require 'capybara/rspec'
@@ -49,7 +50,7 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
-  
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
