d3045950d954deb7fe6a88e5c5f9f6cb92753bab
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-11-04 12:08:50 +0000

    spec_helper error

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require_relative './models/link'
 
 
-# class BookmarkManager < Sinatra::Base
-  
+class BookmarkManager < Sinatra::Base
+
   get '/' do
+    erb(:index)
   end
+
+  get '/links' do
+    @links = Link.all
+    erb(:links)
+  end
+
+run! if app_file == $0
+
+end
